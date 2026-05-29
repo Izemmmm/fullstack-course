e47187f77798883a24f801a22030ee4c620e6805
Izemmmm <2003volkovi@gmail.com>
2026-05-29 11:55:57 +0200

    unicafe final

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -38,25 +38,38 @@ function Button({onClick, text}) {
 
 function Statistics({good, neutral, bad}) {
   const all = good + neutral + bad;
+  if (all === 0) {
+    return (
+      <p>No feedback yet</p>
+    )
+  }
+
   const average = all ? (good - bad) / all : 0;
   const positive = all ? 100 / all * good : 0;
 
   return (
     <div>
       <h2>Statistics:</h2>
-      <Record text="good" value={good} />
-      <Record text="neutral" value={neutral} />
-      <Record text="bad" value={bad} />
-      <Record text="all" value={all} />
-      <Record text="average" value={average} />
-      <Record text="positive" postText="%" value={positive} />
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={all} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" postText="%" value={positive} />
+        </tbody>
+      </table>
     </div>
   );
 }
 
-function Record({text, postText, value}) {
+function StatisticLine({text, postText, value}) {
   return (
-    <p>{text}: {value}{postText}</p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}{postText}</td>
+    </tr>
   );
 }
 
