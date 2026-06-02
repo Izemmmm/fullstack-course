5f0fb6abb8d6e6af71bd4b96982d479b0bd4a135
Izemmmm <2003volkovi@gmail.com>
2026-06-02 22:17:46 +0200

    this bracnh is only for Render deploy

--- part2/countries/src/components/Search.jsx
+++
@@ -1,8 +0,0 @@
-export default function Search({onSearch, value}) {
-  return (
-    <div>
-      find countries 
-      <input style={{marginLeft: 5}} onChange={onSearch} placeholder="Search..." value={value} />
-    </div>
-  );
-}
--- part2/countries/src/components/WeatherInfo.jsx
+++
@@ -1,9 +0,0 @@
-export default function WeatherInfo({temp, wind, image}){
-  return (
-    <div>
-      <p>Temperature {temp} Celsius</p>
-      <img src={image} />
-      <p>Wind {wind} m/s</p>
-    </div>
-  );
-}
