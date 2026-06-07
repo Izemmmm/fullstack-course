8926e92c59912165198763edaa324740b8255342
Izemmmm <2003volkovi@gmail.com>
2026-06-07 08:45:03 +0200

    Secret removed

---
+++ part4/blogList/src/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import blogController from './controllers/blogController.js';
+
+dotenv.config();
+
+const mongoUrl = process.env.MONGO_URI;
+mongoose.connect(mongoUrl, { family: 4 })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(e => console.log('Connection failed:', e));
+
+const app = express();
+
+app.use(express.json());
+app.use('/api/blogs', blogController);
+
+export default app;
